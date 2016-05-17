a94c0e13f22fa85255e4faf3b140985c8f33b643
PhyChoLee <962109025@qq.com>
2016-05-17 23:04:34 +0800

    完成了：应用卸载、运行、分享、详情。其中详情功能失败，原因暂时不详。

--- app/src/main/java/com/llf/mobilesafe/activity/AppManagerActivity.java
+++ app/src/main/java/com/llf/mobilesafe/activity/AppManagerActivity.java
@@ -1,18 +1,27 @@
 package com.llf.mobilesafe.activity;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.net.Uri;
 import android.os.Environment;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
 import android.text.format.Formatter;
+import android.view.Gravity;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.ScaleAnimation;
+import android.widget.AbsListView;
+import android.widget.AdapterView;
 import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.PopupWindow;
 import android.widget.TextView;
 
 import com.lidroid.xutils.ViewUtils;
@@ -24,7 +33,7 @@ import com.llf.mobilesafe.engine.AppInfos;
 import java.util.ArrayList;
 import java.util.List;
 
-public class AppManagerActivity extends Activity {
+public class AppManagerActivity extends Activity implements View.OnClickListener {
     //使用XUtils的注解
     @ViewInject(R.id.tv_rom)
     private TextView tv_rom;
@@ -32,10 +41,14 @@ public class AppManagerActivity extends Activity {
     private TextView tv_sd;
     @ViewInject(R.id.list_app)
     private ListView list_app;
+    @ViewInject(R.id.tv_appbar)
+    private TextView tv_appbar;
 
     private List<AppInfo> appInfos;
     private List<AppInfo> userAppInfos;
     private List<AppInfo> systemAppInfos;
+    private PopupWindow popupWindow;
+    private AppInfo clickAppInfo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -187,5 +200,129 @@ public class AppManagerActivity extends Activity {
         tv_rom.setText("内存可用: "+Formatter.formatFileSize(this, romFreeSpace));
         tv_sd.setText("SD卡可用: "+Formatter.formatFileSize(this, sdFreeSpace));
 
+        //设置滚动监听
+        list_app.setOnScrollListener(new AbsListView.OnScrollListener() {
+            @Override
+            public void onScrollStateChanged(AbsListView view, int scrollState) {
+
+            }
+
+            @Override
+            public void onScroll(AbsListView view, int firstVisibleItem, int visibleItemCount, int totalItemCount) {
+                dismissPopup();
+
+                if (userAppInfos != null && systemAppInfos != null){
+                    if (firstVisibleItem > userAppInfos.size()){
+                        //系统应用
+                        tv_appbar.setText("系统程序("+systemAppInfos.size()+")");
+                    }else {
+                        //用户应用
+                        tv_appbar.setText("用户程序("+userAppInfos.size()+")");
+                    }
+                }
+            }
+        });
+
+        list_app.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Object item = list_app.getItemAtPosition(position);
+                if (item != null && item instanceof AppInfo){
+                    clickAppInfo =   (AppInfo) item;
+
+                    View contentView = View.inflate(AppManagerActivity.this, R.layout.item_popup_app, null);
+
+                    LinearLayout  ll_uninstall = (LinearLayout) contentView.findViewById(R.id.ll_uninstall);
+                    LinearLayout  ll_run = (LinearLayout) contentView.findViewById(R.id.ll_run);
+                    LinearLayout  ll_share = (LinearLayout) contentView.findViewById(R.id.ll_share);
+                    LinearLayout  ll_detail = (LinearLayout) contentView.findViewById(R.id.ll_detail);
+
+                    //将此Activity实现监听方法
+                    ll_uninstall.setOnClickListener(AppManagerActivity.this);
+                    ll_run.setOnClickListener(AppManagerActivity.this);
+                    ll_share.setOnClickListener(AppManagerActivity.this);
+                    ll_detail.setOnClickListener(AppManagerActivity.this);
+
+                    if (popupWindow != null && popupWindow.isShowing()){
+                        popupWindow.dismiss();
+                        popupWindow = null;
+                    }else {
+                        //-2表示wrap_content
+                        popupWindow = new PopupWindow(contentView,-2,-2);
+                        popupWindow.setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                        int[] location = new int[2];
+                        //获取此view在window上的位置
+                        view.getLocationInWindow(location);
+                        popupWindow.showAtLocation(parent, Gravity.TOP+Gravity.LEFT,70,location[1]);
+
+                        //设置动画
+                        ScaleAnimation sa = new ScaleAnimation(0.5f, 1f, 0.5f, 1f,
+                                Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
+                        sa.setDuration(500);
+                        contentView.startAnimation(sa);
+                    }
+                }
+            }
+        });
+    }
+
+    /**
+     * 监听
+     * @param v
+     */
+    public void onClick(View v){
+        switch (v.getId()){
+            case R.id.ll_uninstall:
+                //卸载
+                Intent uninstallIntent = new Intent("android.intent.action.DELETE");
+                uninstallIntent.setData(Uri.parse("package:" + clickAppInfo.getAppPackageName()));
+                startActivity(uninstallIntent);
+                dismissPopup();
+                break;
+
+            case R.id.ll_run:
+                //运行
+                Intent runIntent = this.getPackageManager().getLaunchIntentForPackage(clickAppInfo.getAppPackageName());
+//                runIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_RESET_TASK_IF_NEEDED);
+                this.startActivity(runIntent);
+                dismissPopup();
+                break;
+            case R.id.ll_share:
+                //分享（打开短信编辑页面）
+                Intent shareIntent = new Intent("android.intent.action.SEND");
+                shareIntent.setType("text/plain");
+                shareIntent.putExtra("android.intent.extra.SUBJECT", "f分享");
+                shareIntent.putExtra("android.intent.extra.TEXT",
+                        "Hi！推荐您使用软件：" + clickAppInfo.getAppName()+"下载地址:"+
+                                "https://play.google.com/store/apps/details?id="+clickAppInfo.getAppPackageName());
+                this.startActivity(Intent.createChooser(shareIntent, "分享"));
+                dismissPopup();
+                break;
+            case R.id.ll_detail:
+                //应用详情
+                Intent detailIntent = new Intent();
+                detailIntent.setAction("android.settings.APPLICATION_DETAILS_SETTINGS");
+//                detailIntent.addCategory(Intent.CATEGORY_DEFAULT);
+                detailIntent.setData(Uri.parse("package:" + clickAppInfo.getAppPackageName()));
+                startActivity(detailIntent);
+                dismissPopup();
+                break;
+        }
+    }
+
+    /**
+     * 取消显示Popup
+     */
+    private void dismissPopup() {
+        if (popupWindow != null && popupWindow.isShowing()){
+            popupWindow.dismiss();
+            popupWindow = null;
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        dismissPopup();
     }
 }
